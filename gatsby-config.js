bf79cefe08904fd7c42107e9e67d12c758a0e5a6
trycatchlearn <trycatchlearn@outlook.com>
2019-08-31 09:56:55 +0700

    Updated site url metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `TryCatchLearn`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    siteUrl: `https://loving-goldberg-4082ee`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
